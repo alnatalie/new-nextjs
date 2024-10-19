3269451ae4bcc36b7933da188c4c35d1e5c7b758
Natalie <natasha@email.com>
2024-10-19 11:47:56 +0700

    modified:   pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -27,6 +27,7 @@ export default function Home() {
         className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
       >
         <main className={styles.main}>
+        Привет NextJs!!!
           <Image
             className={styles.logo}
             src="https://nextjs.org/icons/next.svg"
